5d63a0039425f8323af272f63ba1853c4843284c
luanpc <khovidepzai3297@gmail.com>
2021-03-08 10:25:16 +0700

    add comment

--- app/src/main/java/com/luanpc/pokemongo/MainActivity.kt
+++ app/src/main/java/com/luanpc/pokemongo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.luanpc.pokemongo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// dfgdfgdfgdfgfdg
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
